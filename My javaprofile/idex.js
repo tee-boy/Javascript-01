f4105a5ff34ea24cb213dea55a9e07082c6d3a76
Tochukwu Emmanuel <125735714+tee-boy@users.noreply.github.com>
2023-03-31 08:00:36 -0700

    Add files via upload

---
+++ My javaprofile/idex.js
@@ -0,0 +1,6 @@
+windows.alert("Hello, Veiwers My Name is Emmanuel Tochukwu,Am 17years old and am a NIGERIAN.");
+alert("can i know your name");
+let userName = prompt("Your name");
+let firstcharacter = (userName.slice(0,1).toUppercase()) ;
+let restcharacter = (userName.slice(1,20).tolowercase());
+alert (`Hello ${firstcharacter}${restcharacter}`)
